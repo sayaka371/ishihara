a27377ce808128397dfd0516b9203711760c4ee1
sayaka371 <saya2000.213@outlook.jp>
2024-10-18 19:30:34 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,74 @@
+$(function () {
+$('a[href^="#"]').click(function () {
+    // クリックしたaタグのリンクを取得
+    let href = $(this).attr("href");
+    // ジャンプ先のid名をセット hrefの中身が#もしくは空欄なら,htmlタグをセット
+    let target = $(href == "#" || href == "" ? "html" : href);
+    // ページトップからジャンプ先の要素までの距離を取得
+    let position = target.offset().top;
+    // animateでスムーススクロールを行う   ページトップからpositionだけスクロールする
+    // 600はスクロール速度で単位はミリ秒  swingはイージングのひとつ
+    $("html, body").animate({ scrollTop: position + -100 }, 700, "swing");
+    // urlが変化しないようにfalseを返す
+    return false; 
+  });
+
+  let pagetop = $(".arrow");
+  // 最初に画面が表示された時は、トップに戻るボタンを非表示に設定
+  pagetop.hide();
+
+  // スクロールイベント（スクロールされた際に実行）
+  $(window).scroll(function () {
+    // スクロール位置が700pxを超えた場合
+    if ($(this).scrollTop() > 700) {
+      // トップに戻るボタンを表示する
+      pagetop.fadeIn("normal");
+
+      // スクロール位置が700px未満の場合
+    } else {
+      // トップに戻るボタンを非表示にする
+      pagetop.fadeOut("normal");
+    }
+  });
+
+  // クリックイベント（ボタンがクリックされた際に実行）
+  pagetop.click(function () {
+    // 0.5秒かけてページトップへ移動
+    $("body,html").animate({ scrollTop: 0 }, 1000);
+
+    // イベントが親要素へ伝播しないための記述
+    // ※詳しく知りたい方は「イベント　バブリング」または「jQuery バブリング」で調べてみてください
+    return false;
+  });
+
+  $(window).scroll(function () {
+    // fadeinクラスに対して順に処理を行う
+    $("#about,.photo,#contact,#kumiko .container,iframe").each(function () {
+      // スクロールした距離
+      let scroll = $(window).scrollTop();
+      // fadeinクラスの要素までの距離
+      let target = $(this).offset().top;
+      // 画面の高さ
+      let windowHeight = $(window).height();
+      // fadeinクラスの要素が画面下にきてから200px通過した
+      // したタイミングで要素を表示
+      if (scroll > target - windowHeight + 200) {
+        $(this).css("opacity", "1");
+        $(this).css("transform", "translateY(0)");
+      }
+    });
+  });
+
+  $(".btn").on("click",function(){
+    $("header").toggleClass("open")
+  });
+  
+  $(".mask").on("click",function(){
+    $("header").removeClass("open")
+  });
+  
+  $(".open-menu a").on("click",function(){
+    $("header").removeClass("open")
+  });
+
+});
